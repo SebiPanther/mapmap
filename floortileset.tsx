6fd6ab182021b2b613f7eaae57b7966f7c7193d6
Sebi <sebi@chaospott.de>
2020-11-07 22:59:48 +0100

    test

---
+++ floortileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="floortileset" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="64" columns="8">
+ <image source="floortileset.png" width="272" height="272"/>
+</tileset>
